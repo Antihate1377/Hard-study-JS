2b627d782c1be633253d71631e52b5765c3b9dda
Sergey Uglik <antihate1377@gmail.com>
2021-04-09 00:16:55 +0300

    Complete lesson 3 hard

--- script/scripts.js
+++ script/scripts.js
@@ -1,14 +1,29 @@
-let num = 266219,
-    composition = 1;
-    // Перевожу num в строку и разбиваю на массив
-num = String(num).split('');
-    // Через цикл считаю произведение всех цифр в начальном числе
-for (let i = 0; i < num.length; i++) {
-    composition *= num[i];
-    console.log('Current composition is ' + composition + '. Wait please...');
+    // Задаю переменную, которая принимает значения ru или en
+let lang = 'en';
+    // Решение через if
+if (lang === 'ru') {
+    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+} else if (lang === 'en') {
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
 }
-console.log('Final composition is ' + composition + '. Operation is completed.');
-console.log('Exponentiation of the resulting number... Result is ' + composition ** 3);
-    // Перевожу произведение в кубе в строку и извлекаю в переменную twoNumber две первых цифры
-let twoNumber = String(composition ** 3).substring(0, 2);
-console.log('First two numbers of result are ' + twoNumber);
+    // Решение через switch-case
+switch (lang) {
+    case ('ru'):
+        console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+        break;
+    case ('en'):
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+        break;
+}
+    // Решение через многомерный массив
+let arr = {
+    'ru': ['Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'],
+   'en': ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']
+};
+console.log(arr[lang]);
+
+    // Решение второй задачи через несколько тернарных операторов
+let namePerson = 'Сергей',
+    change;
+change = namePerson === 'Артем' ? 'Директор' : namePerson === 'Максим' ? 'Преподаватель' : 'Студент';
+console.log(change);
